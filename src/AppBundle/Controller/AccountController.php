e1507aa3fafc918daece57efd06e4b3c8e44d2fd
Aleksandr Gonchar <shuric2005@mail.ru>
2016-03-01 12:34:40 +0200

    add account, error pages

---
+++ src/AppBundle/Controller/AccountController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class AccountController extends Controller
+{
+    /**
+     * @Route("/account", name="account")
+     * @Template("@App/account/showAccount.html.twig")
+     */
+    public function showAccountAction()
+    {
+        $user = $this->getUser();
+        $em = $this->getDoctrine()->getManager();
+
+        $modules = $em->getRepository('AppBundle:ModuleUser')
+            ->findBy(['user' => $user]);
+
+        $finishModule = $em->getRepository('AppBundle:ModuleUser')
+            ->findBy(['user' => $user, 'isActive' => false]);
+
+        $startModules = $em->getRepository('AppBundle:ModuleUser')
+            ->findBy(['user' => $user, 'isActive' => true]);
+
+        return [
+            'modules' => $modules,
+            'finishModules' => $finishModule,
+            'startModules' => $startModules
+        ];
+    }
+}
